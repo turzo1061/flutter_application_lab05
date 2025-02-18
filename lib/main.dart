10db4dbf3f9e01469879d604114cddf1b04b8e9e
turzo1061 <turzo15-5785@diu.edu.bd>
2025-02-18 11:32:41 +0600

    Overflow control with scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,57 +16,77 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Basic Flutter UI - 02"),
         ),
-        body: Container(
-          height: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    "Hi,",
-                    style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 30,
+        body: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Hi,",
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 30,
+                      ),
                     ),
-                  ),
-                  Text(
-                    " flutter",
-                    style: TextStyle(
+                    Text(
+                      " flutter",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.ac_unit,
                       color: Colors.blue,
-                      fontSize: 20,
+                      size: 40,
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.ac_unit,
-                    color: Colors.blue,
-                    size: 40,
-                  ),
-                  Icon(
-                    Icons.safety_check,
-                    color: Colors.green,
-                    size: 40,
-                  ),
-                  Icon(
-                    Icons.mail,
-                    color: Colors.orange,
-                    size: 40,
-                  ),
-                ],
-              ),
-            ],
+                    Icon(
+                      Icons.safety_check,
+                      color: Colors.green,
+                      size: 40,
+                    ),
+                    Icon(
+                      Icons.mail,
+                      color: Colors.orange,
+                      size: 40,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                buildIconRow(),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+
+  Widget buildIconRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: List.generate(
+        100,
+        (index) => Icon(
+          Iconsax.activity,
+          size: index.toDouble(), // Use index as size
+          color: Color.fromRGBO(index % 256, (index * 2) % 256, (index * 3) % 256, 1), // RGB color based on index
+        ),
+      ),
+    );
+  }
 }
+
